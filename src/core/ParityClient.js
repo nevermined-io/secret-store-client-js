7ce8e6ddd21b3593b7be3cb2289c616e6857419d
ssallam <samer@oceanprotocol.com>
2018-10-09 10:31:33 +0200

    #178: WIP -- some progress.

--- src/core/ParityClient.js
+++ src/core/ParityClient.js
@@ -1,22 +1,33 @@
+import add0xPrefix from './util'
+const jayson = require('jayson')
+
 export default class ParityClient {
-    constructor(url) {
-        this.url = url
-        this.web3 = null
+    constructor(host, port, address, password) {
+        this.host = host
+        this.port = port
+        this.address = address
+        this.password = password
+        this.rpcClient = jayson.client.http({port: this.port})
     }
 
-    signDocumentKeyId() {
-        return ''
+    signDocumentKeyId(documentKeyId) {
+        return sendJsonRpcRequest(this.rpcClient, 'secretstore_signRawHash', [this.address, this.password, documentKeyId])
     }
 
-    generateDocumentKeyFromKey() {
-        return ''
+    generateDocumentKeyFromKey(serverKey) {
+        return sendJsonRpcRequest(this.rpcClient, 'secretstore_generateDocumentKey', [this.address, this.password, serverKey])
+
     }
 
-    encryptDocument() {
-        return ''
+    encryptDocument(encryptedKey, document) {
+        // `document` must be encoded in hex when sent to encryption
+        return sendJsonRpcRequest(this.rpcClient, 'secretstore_encrypt',
+            [this.address, this.password, encryptedKey, add0xPrefix(Buffer(document).toString('hex'))])
     }
 
-    decryptDocument() {
-        return ''
+    decryptDocument(decryptedSecret, commonPoint, decryptShadows, encryptedDocument) {
+        return sendJsonRpcRequest(this.rpcClient, 'secretstore_shadowDecrypt',
+            [this.address, this.password,
+             decryptedSecret, commonPoint, decryptShadows, encryptedDocument])
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import SecretStore from './SecretStore'
-import ParityClient from './ParityClient'
+import SecretStore from './core/SecretStore'
+import ParityClient from './core/ParityClient'
 
-export default class DocumentManager {
+export default class SecretStoreDocumentHandler {
     constructor(ssUrl, evmUrl, address, password) {
         this.secretStore = SecretStore(ssUrl, 1),
         this.parityClient = ParityClient(evmUrl, address, password)
     }
 
-    publishDocument(documentId, document, threshold) {
+    createDocumentStore(documentId, document, threshold) {
         return ''
     }
 
