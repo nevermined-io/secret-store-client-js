3bfb71f0d56c0d44660a7754973f883794a571be
Sebastian Gerske <sebastian.gerske@gmail.com>
2018-10-22 17:08:08 +0200

    use docker parity for unit tests

--- parity/parity.sh
+++ parity/parity.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-parity --chain dev account import UTC--2018-10-22T13-20-36Z--46ece274-a8e8-d274-019b-0377b67f7ab7.json
-parity --chain dev --jsonrpc-apis secretstore --jsonrpc-port 9545
+docker run -d \
+    --volume $(pwd)/keys/:/root/.local/share/io.parity.ethereum/keys/DevelopmentChain:ro \
+    --name secretstore-parity \
+    -p 9545:8545 \
+    parity/parity:stable \
+    --chain dev \
+    --jsonrpc-interface 0.0.0.0 \
+    --jsonrpc-apis secretstore
