612857b3f5d0475c159b7b5603ac486046e7db65
Sebastian Gerske <sebastian.gerske@gmail.com>
2018-10-23 12:44:17 +0200

    fixed partiy node

--- parity/parity.sh
+++ parity/parity.sh
@@ -3,10 +3,11 @@
 cd ./parity/
 
 docker run -it -d \
-    --volume $(pwd)/keys/:/root/.local/share/io.parity.ethereum/keys/DevelopmentChain/:ro \
+    --volume $(pwd)/keys:/home/parity/.local/keys/DevelopmentChain:ro \
     --name secretstore-parity \
     -p 9545:8545 \
     parity/parity:stable \
     --chain dev \
     --jsonrpc-interface 0.0.0.0 \
-    --jsonrpc-apis secretstore
+    --jsonrpc-apis secretstore \
+    --keys-path /home/parity/.local/keys
